cb62f7283da6a114e53148bd9e79c52c41d79dac
alial <sultan.shi1212@gmail.com>
2024-04-24 10:54:35 +0800

    added options

--- Resturant/Adding_manager.Designer.cs
+++ Resturant/Adding_manager.Designer.cs
@@ -41,9 +41,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(325, 87);
+            this.label1.Location = new System.Drawing.Point(433, 107);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 38);
+            this.label1.Size = new System.Drawing.Size(161, 49);
             this.label1.TabIndex = 0;
             this.label1.Text = "USERNAME";
             // 
@@ -51,9 +52,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(325, 172);
+            this.label2.Location = new System.Drawing.Point(433, 212);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 38);
+            this.label2.Size = new System.Drawing.Size(162, 49);
             this.label2.TabIndex = 1;
             this.label2.Text = "PASSWORD";
             // 
@@ -61,31 +63,35 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(346, 253);
+            this.label3.Location = new System.Drawing.Point(461, 311);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 38);
+            this.label3.Size = new System.Drawing.Size(100, 49);
             this.label3.TabIndex = 2;
             this.label3.Text = "EMAIL";
             // 
             // username_mang1
             // 
-            this.username_mang1.Location = new System.Drawing.Point(302, 137);
+            this.username_mang1.Location = new System.Drawing.Point(403, 169);
+            this.username_mang1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.username_mang1.Name = "username_mang1";
-            this.username_mang1.Size = new System.Drawing.Size(170, 20);
+            this.username_mang1.Size = new System.Drawing.Size(225, 22);
             this.username_mang1.TabIndex = 3;
             // 
             // Password_manag1
             // 
-            this.Password_manag1.Location = new System.Drawing.Point(302, 213);
+            this.Password_manag1.Location = new System.Drawing.Point(403, 262);
+            this.Password_manag1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Password_manag1.Name = "Password_manag1";
-            this.Password_manag1.Size = new System.Drawing.Size(170, 20);
+            this.Password_manag1.Size = new System.Drawing.Size(225, 22);
             this.Password_manag1.TabIndex = 4;
             // 
             // Email_manang1
             // 
-            this.Email_manang1.Location = new System.Drawing.Point(302, 294);
+            this.Email_manang1.Location = new System.Drawing.Point(403, 362);
+            this.Email_manang1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Email_manang1.Name = "Email_manang1";
-            this.Email_manang1.Size = new System.Drawing.Size(170, 20);
+            this.Email_manang1.Size = new System.Drawing.Size(225, 22);
             this.Email_manang1.TabIndex = 5;
             // 
             // button1
@@ -93,19 +99,21 @@
             this.button1.BackColor = System.Drawing.SystemColors.MenuText;
             this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Tan;
-            this.button1.Location = new System.Drawing.Point(278, 339);
+            this.button1.Location = new System.Drawing.Point(371, 417);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(216, 39);
+            this.button1.Size = new System.Drawing.Size(288, 48);
             this.button1.TabIndex = 6;
             this.button1.Text = "ADD NEW MANAGER";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Adding_manager
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Sienna;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Email_manang1);
             this.Controls.Add(this.Password_manag1);
@@ -113,6 +121,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Adding_manager";
             this.Text = "Adding_manager";
             this.ResumeLayout(false);
