e88a9e26c1ba99dcff0a9a49e26cd6b6d641e388
alial <sultan.shi1212@gmail.com>
2024-04-30 11:16:04 +0800

    added home button on customer and manager

--- Resturant/Adding_manager.Designer.cs
+++ Resturant/Adding_manager.Designer.cs
@@ -35,6 +35,7 @@
             this.Password_manag1 = new System.Windows.Forms.TextBox();
             this.Email_manang1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -108,12 +109,23 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "HOME";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Adding_manager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Sienna;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Email_manang1);
             this.Controls.Add(this.Password_manag1);
@@ -138,5 +150,6 @@
         private System.Windows.Forms.TextBox Password_manag1;
         private System.Windows.Forms.TextBox Email_manang1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
