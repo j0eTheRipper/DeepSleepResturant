e6a1093f3cd3053a54fce75d6f592cc238d948a1
alial <sultan.shi1212@gmail.com>
2024-05-07 11:50:25 +0800

    change name

--- Resturant/AddUser.Designer.cs
+++ Resturant/AddUser.Designer.cs
@@ -108,7 +108,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(288, 48);
             this.button1.TabIndex = 6;
-            this.button1.Text = "Add User";
+            this.button1.Text = "Add user";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
