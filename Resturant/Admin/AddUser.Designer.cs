bcfda2582181e078d90473643dfe2c91952a2792
alial <sultan.shi1212@gmail.com>
2024-05-07 15:33:28 +0800

    Add some options

---
+++ Resturant/Admin/AddUser.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Resturant
+{
+    partial class AddUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnAdd_user = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rdoManager = new System.Windows.Forms.RadioButton();
+            this.rdoCustomer = new System.Windows.Forms.RadioButton();
+            this.rdoChef = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(320, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USERNAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(320, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PASSWORD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(341, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "EMAIL";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(298, 82);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(170, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(298, 158);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(170, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(298, 239);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(170, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // btnAdd_user
+            // 
+            this.btnAdd_user.BackColor = System.Drawing.SystemColors.MenuText;
+            this.btnAdd_user.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd_user.ForeColor = System.Drawing.Color.Tan;
+            this.btnAdd_user.Location = new System.Drawing.Point(278, 339);
+            this.btnAdd_user.Name = "btnAdd_user";
+            this.btnAdd_user.Size = new System.Drawing.Size(216, 39);
+            this.btnAdd_user.TabIndex = 6;
+            this.btnAdd_user.Text = "Add user";
+            this.btnAdd_user.UseVisualStyleBackColor = false;
+            this.btnAdd_user.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 32);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "HOME";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rdoManager
+            // 
+            this.rdoManager.AutoSize = true;
+            this.rdoManager.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoManager.Location = new System.Drawing.Point(260, 297);
+            this.rdoManager.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdoManager.Name = "rdoManager";
+            this.rdoManager.Size = new System.Drawing.Size(81, 32);
+            this.rdoManager.TabIndex = 8;
+            this.rdoManager.TabStop = true;
+            this.rdoManager.Text = "Manager";
+            this.rdoManager.UseVisualStyleBackColor = true;
+            this.rdoManager.CheckedChanged += new System.EventHandler(this.rdoManager_CheckedChanged);
+            // 
+            // rdoCustomer
+            // 
+            this.rdoCustomer.AutoSize = true;
+            this.rdoCustomer.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoCustomer.Location = new System.Drawing.Point(434, 297);
+            this.rdoCustomer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdoCustomer.Name = "rdoCustomer";
+            this.rdoCustomer.Size = new System.Drawing.Size(87, 32);
+            this.rdoCustomer.TabIndex = 9;
+            this.rdoCustomer.TabStop = true;
+            this.rdoCustomer.Text = "Customer";
+            this.rdoCustomer.UseVisualStyleBackColor = true;
+            // 
+            // rdoChef
+            // 
+            this.rdoChef.AutoSize = true;
+            this.rdoChef.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoChef.Location = new System.Drawing.Point(352, 297);
+            this.rdoChef.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdoChef.Name = "rdoChef";
+            this.rdoChef.Size = new System.Drawing.Size(58, 32);
+            this.rdoChef.TabIndex = 10;
+            this.rdoChef.TabStop = true;
+            this.rdoChef.Text = "Chef";
+            this.rdoChef.UseVisualStyleBackColor = true;
+            // 
+            // AddUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rdoChef);
+            this.Controls.Add(this.rdoCustomer);
+            this.Controls.Add(this.rdoManager);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAdd_user);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddUser";
+            this.Text = "Adding_manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnAdd_user;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton rdoManager;
+        private System.Windows.Forms.RadioButton rdoCustomer;
+        private System.Windows.Forms.RadioButton rdoChef;
+    }
+}
--- Resturant/ManagerMainPage.Designer.cs
+++ Resturant/ManagerMainPage.Designer.cs
@@ -87,6 +87,7 @@
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "ManagerMainPage";
             this.Text = "ManagerMainPage";
+            this.Load += new System.EventHandler(this.ManagerMainPage_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
