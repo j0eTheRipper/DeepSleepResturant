a3972e5443691cf404971df78c53742608494211
FAISAL MOHAMMED EZZADDIN SAIF AHMED <TP077402@mail.apu.edu.my>
2024-05-13 10:20:23 +0800

    added ingredients table , some back end work, and form editing

--- Resturant/chef/Ingredients Inventory.Designer.cs
+++ Resturant/chef/Ingredients Inventory.Designer.cs
@@ -28,29 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
-            this.txtDelete = new System.Windows.Forms.TextBox();
-            this.txtEdit = new System.Windows.Forms.TextBox();
-            this.txtAdd = new System.Windows.Forms.TextBox();
-            this.Ingredients = new System.Windows.Forms.ListBox();
-            this.txtSearch = new System.Windows.Forms.TextBox();
-            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtIngredientName = new System.Windows.Forms.TextBox();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.lblIngredientName = new System.Windows.Forms.Label();
+            this.lblIngredientQuantity = new System.Windows.Forms.Label();
+            this.lblIngredientType = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.database1DataSet = new Resturant.Database1DataSet();
+            this.database1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSearch
             // 
             this.btnSearch.BackColor = System.Drawing.Color.Tan;
-            this.btnSearch.Location = new System.Drawing.Point(423, 22);
+            this.btnSearch.Location = new System.Drawing.Point(560, 278);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.Size = new System.Drawing.Size(131, 34);
             this.btnSearch.TabIndex = 23;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // btnBack
             // 
@@ -63,85 +71,37 @@
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // txtDelete
+            // txtQuantity
             // 
-            this.txtDelete.BackColor = System.Drawing.Color.White;
-            this.txtDelete.Location = new System.Drawing.Point(286, 277);
-            this.txtDelete.Name = "txtDelete";
-            this.txtDelete.Size = new System.Drawing.Size(131, 22);
-            this.txtDelete.TabIndex = 21;
+            this.txtQuantity.BackColor = System.Drawing.Color.White;
+            this.txtQuantity.Location = new System.Drawing.Point(159, 209);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(207, 22);
+            this.txtQuantity.TabIndex = 21;
             // 
-            // txtEdit
+            // txtIngredientName
             // 
-            this.txtEdit.BackColor = System.Drawing.Color.White;
-            this.txtEdit.Location = new System.Drawing.Point(149, 277);
-            this.txtEdit.Name = "txtEdit";
-            this.txtEdit.Size = new System.Drawing.Size(131, 22);
-            this.txtEdit.TabIndex = 20;
-            // 
-            // txtAdd
-            // 
-            this.txtAdd.BackColor = System.Drawing.Color.White;
-            this.txtAdd.Location = new System.Drawing.Point(12, 277);
-            this.txtAdd.Name = "txtAdd";
-            this.txtAdd.Size = new System.Drawing.Size(131, 22);
-            this.txtAdd.TabIndex = 19;
-            // 
-            // Ingredients
-            // 
-            this.Ingredients.BackColor = System.Drawing.Color.Tan;
-            this.Ingredients.FormattingEnabled = true;
-            this.Ingredients.ItemHeight = 16;
-            this.Ingredients.Items.AddRange(new object[] {
-            "item1",
-            "item2",
-            "item3",
-            "item4",
-            "item5",
-            "item6",
-            "item7",
-            "item8",
-            "item9",
-            "item10"});
-            this.Ingredients.Location = new System.Drawing.Point(12, 83);
-            this.Ingredients.Name = "Ingredients";
-            this.Ingredients.Size = new System.Drawing.Size(405, 148);
-            this.Ingredients.TabIndex = 18;
-            this.Ingredients.SelectedIndexChanged += new System.EventHandler(this.Ingredients_SelectedIndexChanged);
-            // 
-            // txtSearch
-            // 
-            this.txtSearch.BackColor = System.Drawing.Color.White;
-            this.txtSearch.Location = new System.Drawing.Point(103, 22);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(314, 22);
-            this.txtSearch.TabIndex = 17;
-            // 
-            // lblSearch
-            // 
-            this.lblSearch.AutoSize = true;
-            this.lblSearch.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSearch.Location = new System.Drawing.Point(18, 17);
-            this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(79, 27);
-            this.lblSearch.TabIndex = 16;
-            this.lblSearch.Text = "Search";
+            this.txtIngredientName.BackColor = System.Drawing.Color.White;
+            this.txtIngredientName.Location = new System.Drawing.Point(159, 104);
+            this.txtIngredientName.Name = "txtIngredientName";
+            this.txtIngredientName.Size = new System.Drawing.Size(207, 22);
+            this.txtIngredientName.TabIndex = 19;
             // 
             // btnClear
             // 
             this.btnClear.BackColor = System.Drawing.Color.Tan;
-            this.btnClear.Location = new System.Drawing.Point(12, 400);
+            this.btnClear.Location = new System.Drawing.Point(508, 400);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(131, 34);
             this.btnClear.TabIndex = 15;
-            this.btnClear.Text = "Clear list";
+            this.btnClear.Text = "Clear ";
             this.btnClear.UseVisualStyleBackColor = false;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnDelete
             // 
             this.btnDelete.BackColor = System.Drawing.Color.Tan;
-            this.btnDelete.Location = new System.Drawing.Point(286, 237);
+            this.btnDelete.Location = new System.Drawing.Point(343, 278);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(131, 34);
             this.btnDelete.TabIndex = 14;
@@ -152,7 +112,7 @@
             // btnEdit
             // 
             this.btnEdit.BackColor = System.Drawing.Color.Tan;
-            this.btnEdit.Location = new System.Drawing.Point(149, 237);
+            this.btnEdit.Location = new System.Drawing.Point(194, 278);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(131, 34);
             this.btnEdit.TabIndex = 13;
@@ -163,7 +123,7 @@
             // btnAdd
             // 
             this.btnAdd.BackColor = System.Drawing.Color.Tan;
-            this.btnAdd.Location = new System.Drawing.Point(12, 237);
+            this.btnAdd.Location = new System.Drawing.Point(39, 278);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(131, 34);
             this.btnAdd.TabIndex = 12;
@@ -171,26 +131,86 @@
             this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
             // 
+            // lblIngredientName
+            // 
+            this.lblIngredientName.AutoSize = true;
+            this.lblIngredientName.Location = new System.Drawing.Point(36, 107);
+            this.lblIngredientName.Name = "lblIngredientName";
+            this.lblIngredientName.Size = new System.Drawing.Size(106, 16);
+            this.lblIngredientName.TabIndex = 24;
+            this.lblIngredientName.Text = "Ingredient Name";
+            // 
+            // lblIngredientQuantity
+            // 
+            this.lblIngredientQuantity.AutoSize = true;
+            this.lblIngredientQuantity.Location = new System.Drawing.Point(48, 215);
+            this.lblIngredientQuantity.Name = "lblIngredientQuantity";
+            this.lblIngredientQuantity.Size = new System.Drawing.Size(55, 16);
+            this.lblIngredientQuantity.TabIndex = 26;
+            this.lblIngredientQuantity.Text = "Quantity";
+            // 
+            // lblIngredientType
+            // 
+            this.lblIngredientType.AutoSize = true;
+            this.lblIngredientType.Location = new System.Drawing.Point(36, 160);
+            this.lblIngredientType.Name = "lblIngredientType";
+            this.lblIngredientType.Size = new System.Drawing.Size(101, 16);
+            this.lblIngredientType.TabIndex = 27;
+            this.lblIngredientType.Text = "Ingredient Type";
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(159, 160);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(207, 22);
+            this.txtType.TabIndex = 28;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // database1DataSetBindingSource
+            // 
+            this.database1DataSetBindingSource.DataSource = this.database1DataSet;
+            this.database1DataSetBindingSource.Position = 0;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.BackgroundColor = System.Drawing.Color.Tan;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(454, 90);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.RowHeadersWidth = 51;
+            this.DataGridView.RowTemplate.Height = 24;
+            this.DataGridView.Size = new System.Drawing.Size(334, 150);
+            this.DataGridView.TabIndex = 29;
+            // 
             // Ingredients_Inventory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Sienna;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.lblIngredientType);
+            this.Controls.Add(this.lblIngredientQuantity);
+            this.Controls.Add(this.lblIngredientName);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnBack);
-            this.Controls.Add(this.txtDelete);
-            this.Controls.Add(this.txtEdit);
-            this.Controls.Add(this.txtAdd);
-            this.Controls.Add(this.Ingredients);
-            this.Controls.Add(this.txtSearch);
-            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtIngredientName);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnAdd);
             this.Name = "Ingredients_Inventory";
             this.Text = "Ingredients_Inventory";
+            this.Load += new System.EventHandler(this.Ingredients_Inventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -200,15 +220,18 @@
 
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.TextBox txtDelete;
-        private System.Windows.Forms.TextBox txtEdit;
-        private System.Windows.Forms.TextBox txtAdd;
-        private System.Windows.Forms.ListBox Ingredients;
-        private System.Windows.Forms.TextBox txtSearch;
-        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtIngredientName;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblIngredientName;
+        private System.Windows.Forms.Label lblIngredientQuantity;
+        private System.Windows.Forms.Label lblIngredientType;
+        private System.Windows.Forms.TextBox txtType;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource database1DataSetBindingSource;
+        private System.Windows.Forms.DataGridView DataGridView;
     }
 }
