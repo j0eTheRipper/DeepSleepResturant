824bfd0be52037028543ea8d46a36ff8c6e3b7df
j0eTheRipper <j0etheripper0010@gmail.com>
2024-05-26 09:41:04 +0800

    re-added ammar's part

--- Resturant/Properties/Settings.Designer.cs
+++ Resturant/Properties/Settings.Designer.cs
@@ -36,8 +36,8 @@ namespace Resturant.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\User\\Source\\Repos\\am" +
-            "marbamtraf\\DeepSleepResturant\\Resturant\\Database1.mdf;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\j0eTh\\source\\repos\\R" +
+            "esturant\\Resturant\\Database1.mdf;Integrated Security=True")]
         public string YoussefConnection {
             get {
                 return ((string)(this["YoussefConnection"]));
