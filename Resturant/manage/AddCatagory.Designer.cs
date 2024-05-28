9526edef4ef3e0bc60cc06e4a8d9199896da71a0
j0eTheRipper <j0etheripper0010@gmail.com>
2024-05-28 20:53:57 +0800

    Finished adding catagory and add menu item, now working on ViewAndEditItems form.

---
+++ Resturant/manage/AddCatagory.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Resturant.manage
+{
+    partial class AddCatagory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNewCatagory = new System.Windows.Forms.TextBox();
+            this.btnAddCatagory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNewCatagory
+            // 
+            this.txtNewCatagory.Location = new System.Drawing.Point(13, 45);
+            this.txtNewCatagory.Name = "txtNewCatagory";
+            this.txtNewCatagory.Size = new System.Drawing.Size(171, 20);
+            this.txtNewCatagory.TabIndex = 0;
+            // 
+            // btnAddCatagory
+            // 
+            this.btnAddCatagory.Location = new System.Drawing.Point(12, 71);
+            this.btnAddCatagory.Name = "btnAddCatagory";
+            this.btnAddCatagory.Size = new System.Drawing.Size(172, 23);
+            this.btnAddCatagory.TabIndex = 1;
+            this.btnAddCatagory.Text = "Add";
+            this.btnAddCatagory.UseVisualStyleBackColor = true;
+            this.btnAddCatagory.Click += new System.EventHandler(this.btnAddCatagory_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Add new catagory";
+            // 
+            // AddCatagory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 103);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddCatagory);
+            this.Controls.Add(this.txtNewCatagory);
+            this.Name = "AddCatagory";
+            this.Text = "AddCatagory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNewCatagory;
+        private System.Windows.Forms.Button btnAddCatagory;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Resturant/manage/ViewAndEditItems.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Resturant.manage
+{
+    partial class ViewAndEditItems
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(144, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(164, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 199);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ViewAndEditItems
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "ViewAndEditItems";
+            this.Text = "ViewAndEditItems";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
